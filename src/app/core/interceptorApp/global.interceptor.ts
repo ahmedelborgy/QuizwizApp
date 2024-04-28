49be55199d7deea235107f1fd12167ec74f8c548
ahmedelborgy <ahmedelborgy130@gmail.com>
2024-04-28 20:04:38 +0300

    intiial commit

---
+++ src/app/core/interceptorApp/global.interceptor.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class GlobalInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+   
+    const token = localStorage.getItem('userToken');    
+    
+    const baseUrl: string =`https://upskilling-egypt.com:3005/api/`;  
+   
+   
+
+    let Headers = request.clone({
+      
+      setHeaders: {
+        'Authorization': ` ${token}`
+      },
+      url: request.url.includes('assets')? request.url:baseUrl + request.url
+    })
+
+  
+   
+   
+   
+   
+    return next.handle(request);
+  }
+}
