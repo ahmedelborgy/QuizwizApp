9bde204962582c19024cb6df48e8fdc1c49732ad
AhmedYasser-js <ahmedkasso22@gmail.com>
2024-04-30 18:54:53 +0300

    imageAnimation

--- src/app/Modules/auth/components/change-password/change-password.component.ts
+++ src/app/Modules/auth/components/change-password/change-password.component.ts
@@ -7,7 +7,7 @@ import { RegxPassword } from '../login/login.component';
 
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-const RegxPassword: RegExp = /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,20}$/;
+// const RegxPassword: RegExp = /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,20}$/;
 
 
 @Component({
@@ -18,86 +18,55 @@ const RegxPassword: RegExp = /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,20}$
 export class ChangePasswordComponent {
 
 
-  // hidePass = true;
-  // password_type: string = 'text';
-  // see: boolean = true;
-  // is_Massage: string = '';
+  hidePass = true;
+  password_type: string = 'text';
+  see: boolean = true;
+  is_Massage: string = '';
 
-  // constructor(private _AuthService: AuthService,
-  //   private _ToastrService: ToastrService,
-  //   private _Router: Router, public _MatDialog: MatDialog) { }
+  constructor(private _AuthService: AuthService, private _ToastrService: ToastrService, private _Router: Router) { }
 
-  // ngOnInit(): void {
-  // }
-
-
-  // changePasswordForm = new FormGroup({
-
-  //   password: new FormControl(null, [Validators.required, Validators.pattern(RegxPassword), Validators.maxLength(20), Validators.minLength(8)]),
-  //   password_new: new FormControl(null, [Validators.required, Validators.pattern(RegxPassword), Validators.maxLength(20), Validators.minLength(8)]),
-  // })
-
-
-  // handleForm(changePasswordForm: FormGroup) {
-  //   console.log(changePasswordForm.value);
-  //   if (changePasswordForm.valid) {
-
-
-  //     this._AuthService.onLogin(changePasswordForm.value).subscribe({
-
-  //       next: (res) => {
-  //         console.log(res);
-
-  //         localStorage.setItem('token', res.data.refreshToken);
-  //         this.is_Massage = res.message;
-  //       },
-  //       error: (err) => {
-  //         console.log(err);
-  //         this._ToastrService.error(`error : ${this.is_Massage}`);
-  //       },
-  //       complete: () => {
-  //         console.log('login completed');
-  //         this._ToastrService.success(`${this.is_Massage}`)
-  //         this._Router.navigate(['/auth/login'])
-  //       },
-
-  //     })
-  //   }
-  // }
-
-  // toggleSee() {
-  //   this.see = !this.see;
-  //   this.password_type = this.see ? 'text' : 'password';
-  // }
-
-  constructor(private _AuthService:AuthService, private __ToastrService:ToastrService, private _Router:Router){}
-
-  changePasswordForm = new FormGroup({
-    password: new FormControl(null, [Validators.required, Validators.pattern(RegxPassword), Validators.maxLength(20), Validators.minLength(8)]),
-    password_new: new FormControl(null, [Validators.required, Validators.pattern(RegxPassword), Validators.maxLength(20), Validators.minLength(8)])
-  })
-
-  onSubmit(data:FormGroup){
-    this._AuthService.onChangePassword(data.value).subscribe({
-      next:(res)=>{
-       console.log(res);
-       
-      },
-      error:(err:any)=>{
-        this.__ToastrService.error(err.message, 'Error ! ');
-      },
-      complete:()=>{
-        
-        this.__ToastrService.success('Your Password Changed Successfully','Success')
-        this._Router.navigate(['/auth/login'])
-
-
-      }
-    })
+  ngOnInit(): void {
   }
 
 
+  changePasswordForm = new FormGroup({
+
+    password: new FormControl(null, [Validators.required, Validators.pattern(RegxPassword), Validators.maxLength(20), Validators.minLength(8)]),
+    password_new: new FormControl(null, [Validators.required, Validators.pattern(RegxPassword), Validators.maxLength(20), Validators.minLength(8)]),
+  })
 
 
+  handleForm(changePasswordForm: FormGroup) {
+    console.log(changePasswordForm.value);
+    if (changePasswordForm.valid) {
+
+
+      this._AuthService.onLogin(changePasswordForm.value).subscribe({
+
+        next: (res) => {
+          console.log(res);
+
+          localStorage.setItem('token', res.data.refreshToken);
+          this.is_Massage = res.message;
+        },
+        error: (err) => {
+          console.log(err);
+          this._ToastrService.error(`error : ${this.is_Massage}`);
+        },
+        complete: () => {
+          console.log('login completed');
+          // this._ToastrService.success(`${this.is_Massage}`)
+          this._ToastrService.success('Your Password Changed Successfully', 'Success')
+          this._Router.navigate(['/auth/login'])
+        },
+
+      })
+    }
+  }
+
+  toggleSee() {
+    this.see = !this.see;
+    this.password_type = this.see ? 'text' : 'password';
+  }
 
 }
